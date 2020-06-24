bb179478c5cbc50aa2fd6ee3e5515a61c88bbd3f
Piotr Chlebda <piotr.chlebda@ewejsciowki.pl>
2020-06-24 22:53:44 +0200

    Fix server.js

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
-app.use(express.static('./dist/angular-heroku'));
+const express = require('express');
+const path = require('path');
 
-app.listen(process.env.PORT || 8080);
+const app = express();
+
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080)
